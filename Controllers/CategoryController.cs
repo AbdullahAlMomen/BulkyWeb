382c097e6ae8005b57d3aea320eb59f23b188382
AbdullahAlMomen <momenshovon21@gmail.com>
2025-10-15 02:14:08 +0600

    Add Entity Framework Core and Category management
    Updated project to use EF Core 9.0.10 and added
    ApplicationDbContext for category management. Created
    Category model and controller, updated navigation,
    and added migrations for database initialization.
    Implemented Index view to display categories.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
