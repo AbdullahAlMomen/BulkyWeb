41d7a82f79b2541a59d9ea0a77e3860c5d1a1b05
AbdullahAlMomen <momenshovon21@gmail.com>
2025-10-16 01:28:48 +0600

    Add category creation feature and UI enhancements
    Updated CategoryController to include methods for creating categories. Introduced Create.cshtml for category input with a form. Redesigned Index.cshtml to display categories in a card layout with improved responsiveness. Enhanced _Layout.cshtml with Bootstrap Icons and a tabbed navigation for better user experience.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -15,5 +15,18 @@ namespace BulkyWeb.Controllers
             List<Category> categories = _context.Categories.ToList();
             return View(categories);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid) {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
